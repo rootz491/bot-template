d29c1e42f3a89e36b9111a308797e9a8d8bfe93c
rootz491 <karansh491@gmail.com>
2023-02-02 02:42:11 +0530

    configs

---
+++ src/configs/config.example.ts
@@ -0,0 +1,6 @@
+export default {
+  TOKEN:
+    'TEST',
+  GUILD_ID: 'GUILD_ID',
+  CLIENT_ID: 'USER_ID_OF_BOT',
+};
--- src/lib/Bot.ts
+++ src/lib/Bot.ts
@@ -7,6 +7,7 @@ import config from '../configs/config';
 
 export default class Bot {
   public commands = new Collection<string, any>();
+  public config = config;
   private commandsArray: any[] = [];
 
   constructor(public readonly client: Client) {
